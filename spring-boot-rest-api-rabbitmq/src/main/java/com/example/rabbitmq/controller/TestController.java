3eaaae42632706c791d4325bb7bf971c47a79f97
Grégori Fernandes de Lima <54556576+gregoriLima@users.noreply.github.com>
2021-08-07 20:55:19 -0300

    added rabbitMQ_def

--- spring-boot-rest-api-rabbitmq/src/main/java/com/example/rabbitmq/controller/TestController.java
+++ spring-boot-rest-api-rabbitmq/src/main/java/com/example/rabbitmq/controller/TestController.java
@@ -1,5 +1,12 @@
 package com.example.rabbitmq.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +23,7 @@ public class TestController {
 	@Autowired
 	RabbitTemplate rabbitTemplate; // usado para publicar as mensagens
 
-	// testing for publish messages in route, direct, funout and topic exchange
+	// testing for publish messages in route, direct, fanout and topic exchange
 	/*
 	 * @GetMapping ("/test/{name}") public String testApi(@PathVariable("name")
 	 * String name) {
@@ -26,7 +33,7 @@ public class TestController {
 	 * // (queue name, serializable object) rabbitTemplate.convertAndSend("Mobile",
 	 * person); // to route rabbitTemplate.convertAndSend("Direct-Exchange", "tv",
 	 * person); // to direct exchange
-	 * rabbitTemplate.convertAndSend("Funout-Exchange", "", person); // to funout
+	 * rabbitTemplate.convertAndSend("Fanout-Exchange", "", person); // to funout
 	 * exchange rabbitTemplate.convertAndSend("Topic-Exchange", "tv.mobile.ac",
 	 * person); // to topic exchange
 	 * 
@@ -35,9 +42,27 @@ public class TestController {
 
 	// testing for publish messages in Headers-Exchange
 	@GetMapping("/test/{name}")
-	public String testApi(@PathVariable("name") String name) {
+	public String testApi(@PathVariable("name") String name) throws IOException {
 		
 		PersonModel person = new PersonModel(1L, name);
+		
+		// converting person object to bytes
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutput out = new ObjectOutputStream(bos);
+		out.writeObject(person);
+		out.flush();
+		out.close();
+		
+		byte[] byteMessage = bos.toByteArray();
+		bos.close();
+		
+		Message message = MessageBuilder
+				.withBody(byteMessage)
+				.setHeader("item1", "mobile")
+				.setHeader("item2", "television")
+				.build();
+		
+		rabbitTemplate.send("Headers-Exchange", "", message );
 		
 		return "Success";
 	}
