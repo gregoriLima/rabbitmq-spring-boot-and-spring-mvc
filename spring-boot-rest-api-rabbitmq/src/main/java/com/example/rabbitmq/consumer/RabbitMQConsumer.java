8e10bafe0b7897548d65cc211fa647cb55da4bd4
Grégori Fernandes de Lima <54556576+gregoriLima@users.noreply.github.com>
2021-08-07 23:23:42 -0300

    Update RabbitMQConsumer.java

--- spring-boot-rest-api-rabbitmq/src/main/java/com/example/rabbitmq/consumer/RabbitMQConsumer.java
+++ spring-boot-rest-api-rabbitmq/src/main/java/com/example/rabbitmq/consumer/RabbitMQConsumer.java
@@ -1,5 +1,10 @@
 package com.example.rabbitmq.consumer;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +17,20 @@ public class RabbitMQConsumer {
 	// public void getMessage(/*byte[] message*/ PersonModel p) {
 	// System.out.println(p.getName());
 	// }
+	
+	
+	// consumer for Header-Exchange 
+	@RabbitListener(queues = "Mobile")
+	public void getMessage( byte[] message ) throws IOException, ClassNotFoundException {
+
+		ByteArrayInputStream bis = new ByteArrayInputStream(message);
+		ObjectInput in = new ObjectInputStream(bis);
+		PersonModel person = (PersonModel) in.readObject();
+		in.close();
+		bis.close();
+		
+		System.out.println(person.getName());
+		
+	}
 
 }
